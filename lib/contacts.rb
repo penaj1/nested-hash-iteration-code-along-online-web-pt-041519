4bafad39e88c24f07a77b6502a29362aa9ced9a8
Janice Pena <jipena94@gmail.com>
2019-04-26 03:52:12 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,8 +15,8 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-#contacts.each do |person, contact_details_hash|
+  contacts.each do |person, contact_details_hash|
   contact_details_hash.each do |attribute, data|
-      binding.pry
+    binding.pry
   end
 end
